7a022300185dd31ec412e0ebd1741dd59f0418ac
Shin Kim <044apde@gmail.com>
2023-07-15 17:26:29 +0900

    update free split

--- src/parse_input.c
+++ src/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: shikim <shikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 17:41:11 by shikim            #+#    #+#             */
-/*   Updated: 2023/07/15 17:20:03 by shikim           ###   ########.fr       */
+/*   Updated: 2023/07/15 17:25:52 by shikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,25 @@
 
 void	parse_input(char *input)
 {
+	int		i;
 	char	**arr;
 	char	*dangling;
 
+	i = 0;
 	arr = split(input);
-	while (*arr != NULL)
+	if (arr == NULL)
+		return ;
+	while (arr[i] != NULL)
 	{
-		printf("%s\n", *arr);
-		arr++;
+		printf("%s\n", arr[i]);
+		i++;
 	}
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
 	return ;
 }
