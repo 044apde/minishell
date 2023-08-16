816fe53af8656c6154e91fdedd196b3575ac7435
Shin Kim <044apde@gmail.com>
2023-08-16 17:31:21 +0900

    last dance

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: shikim <shikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 20:26:00 by hyungjup          #+#    #+#             */
-/*   Updated: 2023/08/15 15:36:18 by shikim           ###   ########.fr       */
+/*   Updated: 2023/08/16 17:29:03 by shikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,38 @@
 
 void	echo_process(t_token *token_list)
 {
-	while (token_list->next != NULL)
+	int	flag;
+
+	flag = 0;
+	token_list = token_list->next;
+	if (compare_str(token_list->token, "-n") == TRUE)
+		token_list = token_list->next;
+	while (token_list != NULL && token_list->type != PIPE)
 	{
-		if (compare_str(token_list->next->token, "-n") == TRUE)
-			token_list = token_list->next;
-		else if (token_list->next->type == REDIR_IN || \
-					token_list->next->type == REDIR_OUT)
+		if (is_operator3(token_list) == TRUE)
 			token_list = token_list->next->next;
-		else if (token_list->next->type == PIPE)
-			break ;
 		else
 		{
-			ft_putstr_fd(token_list->next->token, 1);
-			if (token_list->next->next != NULL)
-				ft_putstr_fd(" ", 1);
+			write(1, token_list->token, ft_strlen(token_list->token));
+			flag = 1;
+			if (flag == 1)
+				write(1, " ", 1);
 			token_list = token_list->next;
 		}
 	}
+	return ;
 }
 
 void	ft_echo(t_token *token_list)
 {
-	int	i;
-
-	i = 0;
-	if (token_list->next == NULL)
-	{
-		ft_putstr_fd("\n", 1);
-		g_exit_code = 0;
-		return ;
-	}
-	if (compare_str(token_list->next->token, "-n") == TRUE)
-		i = 1;
-	echo_process(token_list);
-	if (i == 0)
-		ft_putstr_fd("\n", 1);
 	g_exit_code = 0;
+	if (token_list->next == NULL)
+		ft_putstr_fd("\n", 1);
+	else
+	{
+		echo_process(token_list);
+	}
+	if (compare_str(token_list->next->token, "-n") == FALSE)
+		ft_putstr_fd("\n", 1);
 	return ;
 }
